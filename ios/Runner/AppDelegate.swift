0b8dec00388a3ade949fbb0591f809b6325d895c
Kichi779 <92625816+Kichi779@users.noreply.github.com>
2026-03-05 23:58:40 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import OneSignalFramework // <-- KRİTİK: Bu satırı ekliyoruz
 
 @main
 class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,11 @@ class AppDelegate: FlutterAppDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // --- ONESIGNAL BAŞLATMA ---
+    // Uygulama açılırken iOS bildirim sistemini OneSignal'a bağlar.
+    OneSignal.initialize("0e1426f3-843b-4e98-8fa2-87ee35839a88", withLaunchOptions: launchOptions)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
